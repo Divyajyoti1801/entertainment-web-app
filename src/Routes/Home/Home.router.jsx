449fa8f2bf8e5546b72c966ccaf6f463818491cc
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-04-05 19:53:02 +0530

    Search bar completed now started working on API

--- src/Routes/Home/Home.router.jsx
+++ src/Routes/Home/Home.router.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import searchIcon from "../../assets/icon-search.svg";
 import "./Home.router.scss";
 
+const defaultSearchField = {
+  search: "",
+};
+
 const Home = () => {
-  return <div className="home">Home</div>;
+  const [searchField, setSearchField] = useState(defaultSearchField);
+  const { search } = searchField;
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setSearchField({ ...searchField, [name]: value });
+  };
+
+  return (
+    <div className="home">
+      <div className="home__search">
+        <img
+          src={searchIcon}
+          alt="Search Icon"
+          className="home__search--icon"
+        />
+        <input
+          className="home__search--input"
+          placeholder="Search for Movies and TV series"
+          name="search"
+          value={search}
+          onChange={onChangeHandler}
+          autoComplete="off"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
